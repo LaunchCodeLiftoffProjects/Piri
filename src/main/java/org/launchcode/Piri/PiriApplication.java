1299911506fffc3aa5e0dfc0d705d6a2e0944670
Your Name <you@example.comgit>
2020-11-30 18:08:02 -0800

    Initial commit.

---
+++ src/main/java/org/launchcode/Piri/PiriApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.Piri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/Piri/PiriApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.Piri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

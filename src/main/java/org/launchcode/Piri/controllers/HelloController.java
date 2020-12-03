736a2b5c6c80b1c3a76740c1300a5c5f93e411a4
Your Name <you@example.comgit>
2020-12-02 19:12:24 -0800

    Authentication controller, added data and dto models, connected to MySQL, and added login/register templates

---
+++ src/main/java/org/launchcode/Piri/controllers/HelloController.java
@@ -0,0 +1,13 @@
+package org.launchcode.Piri.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloController {
+
+        public String hello(){
+
+            return "index";
+        }
+}
+

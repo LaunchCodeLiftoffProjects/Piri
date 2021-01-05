e92de88592a7dff3650a75f5f1decc69d65e2b0e
Your Name <you@example.comgit>
2021-01-05 12:29:42 -0800

    Edited repository

--- src/main/java/org/launchcode/Piri/models/data/ReviewRepository.java
+++ src/main/java/org/launchcode/Piri/models/data/ReviewRepository.java
@@ -1,4 +1,7 @@
 package org.launchcode.Piri.models.data;
 
-public interface ReviewRepository extends CrudRepository<Review,Integer> {
+import org.launchcode.Piri.models.Review;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ReviewRepository extends CrudRepository<Review, Integer> {
 }

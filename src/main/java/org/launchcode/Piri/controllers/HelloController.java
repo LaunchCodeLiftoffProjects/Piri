87f920ff95f726d1a99284a5ae1fe92767e839d1
Your Name <you@example.comgit>
2021-01-13 19:56:43 -0800

    Required login for users

--- src/main/java/org/launchcode/Piri/controllers/HelloController.java
+++ src/main/java/org/launchcode/Piri/controllers/HelloController.java
@@ -12,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
 
@@ -26,6 +27,7 @@ public class HelloController {
     @Autowired
     private ReviewRepository reviewRepository;
 
+    @RequestMapping("home")
     public String hello(){
 
         return "index";

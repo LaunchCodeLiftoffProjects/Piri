22369d7874a541cb19263710814aab10c4dc42d0
themitcharoni <64182946+themitcharoni@users.noreply.github.com>
2021-01-20 15:50:04 -0600

    refactored view-profile.html

--- src/main/java/org/launchcode/Piri/controllers/UserController.java
+++ src/main/java/org/launchcode/Piri/controllers/UserController.java
@@ -1,6 +1,7 @@
 package org.launchcode.Piri.controllers;
 
 
+import com.mysql.cj.protocol.x.XAuthenticationProvider;
 import org.launchcode.Piri.models.User;
 import org.launchcode.Piri.models.data.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
 @Controller
@@ -17,19 +20,23 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
-    @GetMapping("view-profile/{userId}")
-    public String displayViewUserProfile(Model model, @PathVariable int userId) {
+    @Autowired
+    AuthenticationController authenticationController;
 
-        Optional<User> optUser = userRepository.findById(userId);
+    @GetMapping("view-profile")
+    public String displayViewUserProfile(Model model, HttpServletRequest request) {
+
+        HttpSession session = request.getSession();
+        User user = authenticationController.getUserFromSession(session);
+        Optional<User> optUser = userRepository.findById(user.getId());
 
         if (optUser.isPresent()) {
-            User user = (User) optUser.get();
             model.addAttribute("user", user);
             model.addAttribute("title", "${user.username}");
             return "user/view-profile";
         } else {
-            model.addAttribute("title", "'Cannot find user with ID: ' + ${userID} ");
-            model.addAttribute("error", "'Cannot find user with ID: ' + ${userID}");
+            model.addAttribute("title", "'Cannot find user with ID: ' + ${user.getId()} ");
+            model.addAttribute("error", "'Cannot find user with ID: ' + ${user.getId()}");
             return "user/view-profile";
         }
     }
